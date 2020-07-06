dc5c5a990d43ad2fd39072079d4458d3a6642217
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-06 21:40:59 +0000

    build based on cb84f6e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RemoveDateInformation","category":"page"},{"location":"#RemoveDateInformation","page":"Home","title":"RemoveDateInformation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RemoveDateInformation]","category":"page"}]
+}
